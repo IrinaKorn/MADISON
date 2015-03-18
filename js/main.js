8813f8251d203405bc88a159d8e87b312c549472
Irina Kosterina <korn79irina@gmail.com>
2015-03-18 22:39:01 +0200

    psd04
    new progect

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,1],
+      itemsDesktopSmall : [979,1]
+ 
+  });
+ 
+});
+
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel();
+ 
+});
